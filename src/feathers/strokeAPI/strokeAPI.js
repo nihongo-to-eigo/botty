f2f2ec20660bb0ce68d4e5db7a1f8f033859706b
Ronin <rxswyers@gmail.com>
2018-07-02 10:52:23 -0400

    Wittle stroke order updoot
    Add in stroke order command boi.

---
+++ src/feathers/strokeAPI/strokeAPI.js
@@ -0,0 +1,42 @@
+//Feather for the stroke order API
+'use strict';
+module.exports = function feather(requires)
+{
+  //feather obj
+  const feather = {};
+  //set variable for config
+  const config = require(`${process.cwd()}/feathers.json`).kanjiAPI;
+  
+  //requires
+  const snekfetch = require('snekfetch');
+  const urlencode = require('urlencode');
+  
+  //feather functions
+  feather.searchKanji = function(kanji)
+  {
+    return kanjiReq(kanji);
+  }
+  //helper functions
+  const kanjiReq = function(kanji)
+  {
+    return new Promise((resolve, reject) =>
+    {
+      let url = 'https://skurt.me/api/kanji/stroke/' + urlencode(kanji);
+      snekfetch.get(url).set('Content-type', 'application/json').set('authorization', config.key)
+        .then((response) =>
+        {
+          if(response.statusCode === 200)
+          {
+            console.log(response.body);
+            resolve(response.body);
+          }
+          else
+          {
+            reject(response);
+          }
+        }).catch(reject);
+    });
+  }
+
+  return feather;
+};
